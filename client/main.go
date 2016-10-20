e6c62eb4952f25f3480db1294029d202c7a85def
S.S <ss@ss.com>
2016-10-20 18:23:57 +0900

    try fin.

--- client/main.go
+++ client/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	trygrpcgo "github.com/sky0621/try-grpc-go"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	conn, err := grpc.Dial("localhost:5050", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	client := trygrpcgo.NewPersonSenderClient(conn)
+	p := trygrpcgo.Person{Name: "Who?"}
+	r, err := client.SendPerson(context.Background(), &p)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
 }
--- server/main.go
+++ server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net"
+
+	context "golang.org/x/net/context"
+
+	trygrpcgo "github.com/sky0621/try-grpc-go"
+
+	"google.golang.org/grpc"
+)
+
+type pSendServer struct{}
+
+func (s pSendServer) SendPerson(context.Context, *trygrpcgo.Person) (*trygrpcgo.SendPersonReply, error) {
+	log.Println("[SendPerson]START")
+	// log.Println(p.Name)
+	return &trygrpcgo.SendPersonReply{Message: "send OK !?"}, nil
+}
+
+var server trygrpcgo.PersonSenderServer
 
 func main() {
-	fmt.Println("vim-go")
+	addrFlag := flag.String("p", ":5050", "host:port")
+	lis, err := net.Listen("tcp", *addrFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var svr trygrpcgo.PersonSenderServer
+	var pSvr pSendServer
+	svr = pSvr
+
+	grpcServer := grpc.NewServer()
+	trygrpcgo.RegisterPersonSenderServer(grpcServer, svr)
+	grpcServer.Serve(lis)
 }
